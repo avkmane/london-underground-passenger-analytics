dfa3e6531aac267ddcb1ab85b83f64e99ccbf66c
avkmane <avkmane@gmail.com>
2026-08-11 16:35:13 +0100

    Add dbt entry staging model

---
+++ dbt_analytics/models/staging/stg_entries.sql
@@ -0,0 +1,5 @@
+{% set hours = ['H05','H06','H07','H08','H09','H10','H11','H12','H13','H14','H15','H16','H17','H18','H19','H20','H21','H22','H23','H00','H01'] %}
+{% for hour in hours %}
+select Station as station, '{{ hour }}' as hour_code, {{ hour }}::bigint as entries from {{ ref('hourly_entries') }}
+{% if not loop.last %} union all {% endif %}
+{% endfor %}
